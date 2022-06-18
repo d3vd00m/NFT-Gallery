c8bd96b0080d337dddff1849e04e84d505f16d32
d3vd00m <34721259+d3vd00m@users.noreply.github.com>
2022-06-19 01:41:48 +0200

    .

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
-import NFTCard from "./components/nftCard"
+import { NFTCard } from "./components/nftCard"
 
 const Home = () => {
   // Create Two Variables to Store Wallet and Collection Addresses
